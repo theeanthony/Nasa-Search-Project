487f94babaa7f7b47d7287359406251c24a045b8
theeanthony <acontreras.sing@gmail.com>
2023-06-26 18:04:52 -0700

    Initial Commit

---
+++ Nasa Project/Nasa_ProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  Nasa_ProjectApp.swift
+//  Nasa Project
+//
+//  Created by Anthony Contreras on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Nasa_ProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
