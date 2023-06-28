29b02c5bcf977c8d6da08321a8adbeba56aa5e58
theeanthony <acontreras.sing@gmail.com>
2023-06-27 18:30:49 -0700

    Added pagination, added tests testing search, clear, next, prev

---
+++ Nasa Project/Enums/Enum+Error.swift
@@ -0,0 +1,8 @@
+//
+//  Enum+Error.swift
+//  Nasa Project
+//
+//  Created by Anthony Contreras on 6/27/23.
+//
+
+import Foundation
---
+++ Nasa Project/View/Components/SearchBarComponents/PaginationView.swift
@@ -0,0 +1,20 @@
+//
+//  PaginationView.swift
+//  Nasa Project
+//
+//  Created by Anthony Contreras on 6/27/23.
+//
+
+import SwiftUI
+
+struct PaginationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaginationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaginationView()
+    }
+}
